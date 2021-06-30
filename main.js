aac11fd2339278e52c458eefccaeed79bf716a94
sadirov4647 <sadirovizzatillo3@gmail.com>
2021-06-30 19:23:57 +0500

    features commit

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 let status = false
 function show(){
-if(status){
-    document.querySelector('.site-nav__list').style.display = 'block';
-    status =!status
-}else{
-    document.querySelector('.site-nav__list ').style.display = 'none';
-    status =!status
+    if(status){
+        document.querySelector('.site-nav__list').style.display = 'block';
+        status =!status
+    }else{
+        document.querySelector('.site-nav__list ').style.display = 'none';
+        status =!status
+    }
 }
-}
+
+
+
+let elTabsFeatures = document.querySelector('.tabs-features');
+let elTabsFeaturesItem = document.querySelectorAll('.tabs-features__item')
+let elTabsFeaturesLink = document.querySelectorAll('.tabs-features__link');
+let elsPanel = document.querySelectorAll('.features__panel');
+
+if(elTabsFeaturesLink.length > 0){
+    elTabsFeaturesLink.forEach(function(link){
+        link.addEventListener('click', function(evt) {
+            evt.preventDefault();
+
+            elTabsFeaturesItem.forEach(function(item){
+                item.classList.remove('tabs-features__item--active');
+            });
+
+            link.parentElement.classList.add('tabs-features__item--active');
+
+            elsPanel.forEach(function(panel){
+                panel.classList.remove('features__panel-active')
+            });
+
+            document.querySelector(link.getAttribute('href')).classList.add('.features__panel--active');
+        })
+    }
+    )}
